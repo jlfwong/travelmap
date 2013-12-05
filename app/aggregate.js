29f45510109dd453e3483497e4f1bfb9aacc6ef5
Jamie Wong <jamie.lf.wong@gmail.com>
2013-12-05 02:12:46 -0800

    Pie arc cities, lower opacity travel paths

--- app/aggregate.js
+++ app/aggregate.js
@@ -8,11 +8,11 @@ module.exports = function(rawData) {
   var data = [];
   var nameNum = 0;
   _.forOwn(rawData, function(placesForPerson, name) {
-    _.each(placesForPerson, function(place) {
+    _.each(placesForPerson, function(placeRaw) {
       data.push({
         name: name,
-        place: place,
-        promise: cachedGeocode(place)
+        placeRaw: placeRaw,
+        promise: cachedGeocode(placeRaw)
       });
     });
   });
@@ -21,9 +21,11 @@ module.exports = function(rawData) {
     .then(function() {
       var geocodeResults = Array.prototype.slice.apply(arguments);
       _(geocodeResults).each(function(result, index) {
-        data[index].lat = result.lat;
-        data[index].lon = result.lon;
-        data[index].reversePromise = cachedReverseGeocode(result);
+        _.extend(data[index], {
+          lat: result.lat,
+          lon: result.lon,
+          reversePromise: cachedReverseGeocode(result)
+        });
       });
 
       return $.when.apply($.when, _.pluck(data, 'reversePromise'));
@@ -32,26 +34,58 @@ module.exports = function(rawData) {
       var reverseResults = Array.prototype.slice.apply(arguments);
 
       _.each(reverseResults, function(result, index) {
-        data[index].humanized = result;
+        _.extend(data[index], {humanized: result});
       });
-
-      return _.values(_.reduce(data, function(result, d) {
+    })
+    .then(function() {
+      var places = _.values(_.reduce(data, function(result, d) {
         var key = d.humanized;
         var place;
         if (!(place = result[key])) {
-          place = result[key] = {
-            lat: d.lat,
-            lon: d.lon,
-            humanized: d.humanized,
-            city: d.city,
-            country: d.country,
+          place = result[key] = _.extend({
             count: 0,
-            names: []
-          };
+            names: [],
+            countByName: {}
+          }, d);
         }
         place.count++;
         place.names = _.uniq([d.name].concat(place.names));
+        if (!place.countByName[d.name]) {
+          place.countByName[d.name] = 0;
+        }
+        place.countByName[d.name]++;
         return result;
       }, {}));
+
+      var placesPerPerson = _.reduce(places, function(result, place) {
+        return result.concat(_.map(place.names, function(name, index) {
+          return {
+            name: name,
+            nameIndex: index,
+            names: place.names,
+            lat: place.lat,
+            lon: place.lon,
+            humanized: place.humanized,
+            count: place.countByName[name],
+            totalCount: place.count,
+            placeRaw: place.placeRaw
+          };
+        }));
+      }, []);
+
+      var placesByPerson = _.groupBy(data, 'name');
+      var pairsByPerson = _.reduce(placesByPerson, function(result, places, name) {
+        result[name] = _(places)
+          .zip([null].concat(places))
+          .filter(function(x) { return x[0] && x[1]; })
+          .value();
+
+        return result;
+      }, {});
+
+      return {
+        placesPerPerson: placesPerPerson,
+        pairsByPerson: pairsByPerson
+      };
     });
 };
--- app/main.js
+++ app/main.js
@@ -9,65 +9,6 @@ var COLORS = [
   d3.rgb(150, 150, 0)
 ];
 
-var renderPath = function(svg, projection, cities, name, color) {
-  console.log('Loading data for', name);
-
-  var cityCodesPromise = $.when.apply($.when, _.map(cities, function(city) {
-    return cachedGeocode(city);
-  }));
-
-  cityCodesPromise.then(function() {
-    var cityCoords = Array.prototype.slice.apply(arguments);
-
-    console.log('Rendering', name, cityCoords.length);
-
-    var counts = _.countBy(cityCoords, function(x) {
-      return JSON.stringify(x);
-    });
-
-    svg.selectAll(".city." + name)
-      .data(_.uniq(cityCoords))
-      .enter()
-      .append("circle")
-      .attr("fill", color)
-      .attr("fill-opacity", 0.75)
-      .attr("class", "city " + name)
-      .attr("cx", function(d) {
-        return projection([d.lon, d.lat])[0];
-      })
-      .attr("cy", function(d) {
-        return projection([d.lon, d.lat])[1];
-      })
-      .attr("r", function(d) {
-        return 2 + 2 * Math.log(1 + counts[JSON.stringify(d)]);
-      });
-
-    var pairs = _(cityCoords)
-      .zip([null].concat(cityCoords))
-      .filter(function(x) { return x[0] && x[1]; })
-      .value();
-
-    svg.selectAll(".travelpath." + name)
-      .data(pairs)
-      .enter()
-      .append("path")
-      .attr("class", "travelpath " + name)
-      .attr("stroke-opacity", 0.25)
-      .attr("stroke", color)
-      .attr("fill-opacity", 0)
-      .attr("d", function(pair) {
-        var c1 = projection([pair[0].lon, pair[0].lat]);
-        var c2 = projection([pair[1].lon, pair[1].lat]);
-        // TODO(jlfwong): The rx and ry arguments should be scaled relative to
-        // width/height
-        return (
-          "M" + c1[0] + "," + c1[1] +
-          " A800,800 0 0,1 " + c2[0] + "," + c2[1]
-        );
-      });
-  });
-};
-
 module.exports = function() {
   // TODO(jlfwong): This crops part of the map
   var width = $(window).width(),
@@ -108,22 +49,33 @@ module.exports = function() {
         "display": "none"
       });
 
-    aggregate(data).then(function(places) {
-      var placePerPerson = _.reduce(places, function(result, place) {
-        return result.concat(_.map(place.names, function(name, index) {
-          return _.extend({
-            name: name,
-            nameCount: place.names.length,
-            nameIndex: index,
-          }, place);
-        }));
-      }, []);
-
+    aggregate(data).then(function(processed) {
       var nameCounter = uniqueCounter();
       var arc = d3.svg.arc().innerRadius(0);
 
+      _.forOwn(processed.pairsByPerson, function(pairs, name) {
+          svg.selectAll(".travelpath." + name)
+            .data(pairs)
+            .enter()
+            .append("path")
+            .attr("class", "travelpath " + name)
+            .attr("stroke-opacity", 0.25)
+            .attr("stroke", COLORS[nameCounter(name)])
+            .attr("fill-opacity", 0)
+            .attr("d", function(pair) {
+              var c1 = projection([pair[0].lon, pair[0].lat]);
+              var c2 = projection([pair[1].lon, pair[1].lat]);
+              // TODO(jlfwong): The rx and ry arguments should be scaled
+              // relative to width/height
+              return (
+                "M" + c1[0] + "," + c1[1] +
+                " A800,800 0 0,1 " + c2[0] + "," + c2[1]
+              );
+            });
+      });
+
       svg.selectAll(".place")
-        .data(placePerPerson)
+        .data(_.sortBy(processed.placesPerPerson, "totalCount"))
         .enter()
         .append("path")
         .attr({
@@ -136,16 +88,16 @@ module.exports = function() {
             return "translate(" + proj[0] + "," + proj[1] + ")";
           },
           "d": function(d) {
-            var sliceAngle = 2 * Math.PI / d.nameCount;
+            var sliceAngle = 2 * Math.PI / d.names.length;
             return arc({
-              outerRadius: 2 + 2 * Math.log(1 + d.count),
+              outerRadius: 1 + 2 * Math.log(1 + d.totalCount),
               startAngle: d.nameIndex * sliceAngle,
               endAngle: (d.nameIndex + 1) * sliceAngle
             });
           }
         })
         .on("mouseover", function(d) {
-          tooltip.text(d.humanized + " (" + d.names.join(", ") + ")");
+          tooltip.text(d.placeRaw + " (" + d.names.join(", ") + ")");
           tooltip.style("display", "block");
         })
         .on("mousemove", function() {
